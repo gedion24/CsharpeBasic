a235967f48f31f7a1db051932a08da9931a28b00
gedion24 <57101143+gedion24@users.noreply.github.com>
2022-07-10 14:03:48 +0300

    Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//for loop
+int x = 20;
+int y = 18;
+if (x > y)
+{
+    Console.WriteLine("x is greater than y");
+}
+
+// while loop
+
+int l = 0;
+while (l < 5)
+{
+    Console.WriteLine(l);
+    l++;
+}
+
+//foreach
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+foreach (string i in cars)
+{
+    Console.WriteLine(i);
+}
+//arry
+string[] car = { "Volvo", "BMW", "Ford" };
+Console.WriteLine(car[0]);
